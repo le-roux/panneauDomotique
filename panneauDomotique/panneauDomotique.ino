01e091e4a1d176c8856259e712d812ab7d00d283
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.org>
2018-05-10 05:52:20 +0200

    WIP

--- panneauDomotique/panneauDomotique.ino
+++ panneauDomotique/panneauDomotique.ino
@@ -1,13 +1,13 @@
 #include <SPI.h>
 #include "led.h"
 
-#define LED_NB 2
+#define LED_NB 3
 
 uint8_t r[LED_NB] = {0};
 uint8_t g[LED_NB] = {0};
 uint8_t b[LED_NB] = {0};
 int val = 0;
-int sign = 1;
+int sign = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,12 +25,32 @@ void loop() {
   // put your main code here, to run repeatedly:
   delay(200);
 
-    r[sign] = 0xFF;
-    g[sign] = 0;
-    r[1 - sign] = 0;
-    g[1 - sign] = 0xFF;
+  if ((sign % 2) == 0) {
+    r[0] = 0x00;
+    g[0] = 0x00;
+    b[0] = 0xEE;
+  } else {
+    r[0] = 0x0;
+    g[0] = 0x0;
+    b[0] = 0x0;
+  }
 
-  sign = 1 - sign;
+  if ((sign % 5) == 0) {
+    g[1] = 0xFF;
+    g[2] = 0xFF;
+    r[1] = 0xFF;
+    r[2] = 0xFF;
+    b[1] = 0xFF;
+    b[2] = 0xFF;
+ } else {
+    g[1] = 0x77;
+    g[2] = 0x77;
+    r[1] = 0xFF;
+    r[2] = 0xFF;
+    b[1] = 0x00;
+    b[2] = 0x00;
+ }
+
+  sign++;
   sendPattern(r, g, b, LED_NB);
-
 }
